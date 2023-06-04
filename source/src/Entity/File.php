46ed4c4d6a138cc393151cffb342e147f95dff41
Ahmed <ahmedmust@hotmail.fr>
2023-06-04 16:57:48 +0200

    initial commit api-platform simple example

---
+++ source/src/Entity/File.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\FileRepository;
+use ApiPlatform\Metadata\Post;
+use App\Controller\ExportController;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FileRepository::class)]
+#[ApiResource(operations: [
+    new Post(
+        name: 'publication', 
+        uriTemplate: '/export', 
+        controller: ExportController::class
+    )
+])]
+class File
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $name = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
